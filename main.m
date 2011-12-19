22bed41049000bb71b80227191d97a5f9869606d
Atul Kumar <atulcst@gmail.com>
2011-12-19 13:15:21 -0800

    removed extra comments

--- main.m
+++ main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  CoreDataDAO
-//
-//  Created by Kumar, Atul on 12/19/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 int main(int argc, char *argv[]) {
